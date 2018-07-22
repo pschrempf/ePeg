edfdef2c37b9e3349e16162185aff57dc3525e1a
Gergely Flamich <gf38@st-andrews.ac.uk>
2018-07-22 10:36:24 +0100

    Commiting the initial sketch

--- Exhibition/frontend/js/exhibition.js
+++ Exhibition/frontend/js/exhibition.js
@@ -1,15 +1,41 @@
 // Start doing things when the page has loaded completely.
 document.addEventListener("DOMContentLoaded", (e) => {
 
-    var chart1 = barchart(window.innerWidth - 120, 600, 7);
+    var phase = 0;
 
-    chart1("#visualisation");
+    var chart1 = barchart(.3 * window.innerWidth, 600, 7);
+    var chart2 = barchart(.3 * window.innerWidth, 600, 7);
+
+    chart1("#vis1");
+    chart2("#vis2");
 
     // Make connection
     var socket = io();
 
     socket.on('peg_info', function(data){
-        chart1.update_chart(data);
+            chart1.update_chart(data);
+            chart2.update_chart(data);
     });
+
+    d3.select("#haha")
+        .on("click", () => {
+            switch(phase){
+            case 0:
+                d3.select("#header")
+                    .transition().duration(750)
+                    .style("height", (window.innerHeight) + "px")
+                    .on('end', () => {
+                        d3.select("#header")
+                            .transition().duration(750)
+                            .style("height", "100px");
+                    });
+                    phase = 1;
+                break;
+
+            case 1:
+                phase = 0;
+                break;
+            }
+        });
 
 });
--- Exhibition/frontend/js/visualisation.js
+++ Exhibition/frontend/js/visualisation.js
@@ -1,5 +1,27 @@
+/* School of Medicine colours:
+
+   blue: #5f819e
+   light blue: #6dbcdb
+   gray: #757575
+
+   Bathing in the Sun
+   dark orange: #c14b22	(193,75,34)
+   light orange: #f28143	(242,129,67)
+   yellow: #fde033	(253,224,51)
+   deep sea: #106f7c	(16,111,124)
+   light blue: #2199c3	(33,153,195)
+
+   Muted neutral tones
+   navy: #27567b	(39,86,123)
+   dark red: #b63b3b	(182,59,59)
+   yellow: #f9c414	(249,196,20)
+   beige: #f7ebd8	(247,235,216)
+   dark gray: #505050	(80,80,80)
+*/
+
+
 // Closure that when called will create a barchart object
-function barchart(width, height, num_bars){
+function barchart(width, height, num_bars, palette){
 
     // References we will hold on to so that internal functions can use them
     var vis;
@@ -9,6 +31,15 @@ function barchart(width, height, num_bars){
     const bar_width = width / (num_bars + 2);
     const margin = {top: 30, right: 10, bottom: 40, left: 60};
 
+    const default_palette = {background: "#f7ebd8",
+                             right: "#27567b",
+                             left: "#b63b3b",
+                             frame: "#505050",
+                             emphasis: "#f9c414"
+                            };
+
+    // If the user has not provided us with a palette, use the default one
+    palette = palette || default_palette;
 
     var x_scale;
     var y_scale;
@@ -33,14 +64,19 @@ function barchart(width, height, num_bars){
             .attr("class", "axis")
             .call(x_axis);
 
+        d3.selectAll("g.tick line").remove();
+        d3.select(".domain").remove();
+
         vis.append("g")
             .attr("class", "axis")
-            .attr("transform", "translate(0, " + (-height) + ")")
-            .call(y_axis);
+            .attr("transform", "translate(0, " + (-height) + ")");
+            //.call(y_axis);
 
         // Create a group for the bars
         bars = vis.append("g")
             .attr("class", "bar_group");
+
+        create_legend();
 
     }
 
@@ -58,14 +94,44 @@ function barchart(width, height, num_bars){
 
         // Create linear scale for the peg deltas
         y_scale = d3.scaleLinear()
-            .domain([0, 2000])
+            .domain([0, 1000])
             .range([0, height]);
 
         // Now create the axes
         x_axis = d3.axisBottom(x_scale)
             .tickFormat((d) => "Peg " + d);
-        y_axis = d3.axisLeft(y_scale)
+
+        /* y_axis = d3.axisLeft(y_scale)
             .tickFormat((d) => 2000 - d);
+        */
+
+    };
+
+    var create_legend = function (){
+
+        var legend_box = vis.append("g")
+            .attr("transform", "translate(" + 80 + "," + -height + ")");
+
+        legend_box.append("svg:image")
+            .attr("width", 40)
+            .attr("height", 100)
+            .attr("xlink:href", "img/left_hand.png");
+
+        legend_box.append("text")
+            .attr("x", 60)
+            .attr("y", 60)
+            .text(() => "Left Hand");
+
+        legend_box.append("svg:image")
+            .attr("x", 250)
+            .attr("width", 40)
+            .attr("height", 100)
+            .attr("xlink:href", "img/right_hand.png");
+
+        legend_box.append("text")
+            .attr("x", 310)
+            .attr("y", 60)
+            .text(() => "Right Hand");
 
     };
 
@@ -73,6 +139,9 @@ function barchart(width, height, num_bars){
 
         var deltas = data.measurements.pegDeltas;
         var hand_used = data.handUsed;
+
+        console.log(deltas);
+        console.log(hand_used);
 
         var new_bars = bars.selectAll("rect." + hand_used);
 
