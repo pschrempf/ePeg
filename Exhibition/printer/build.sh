4bbbbe4135d81d1a746150b81503f0aad51c22fc
Gergely Flamich <gf38@st-andrews.ac.uk>
2018-08-21 18:41:37 +0000

    Changed label printer compilation file. Printer works!

--- Exhibition/printer/build.sh
+++ Exhibition/printer/build.sh
@@ -1,3 +1,1 @@
-g++ labelPrint.cpp -lcups -Wl,-z,relro -lgssapi_krb5 -lkrb5 -lk5crypto -lcom_err -lgnutls -L/lib/arm-linux-gnueabihf -lgcrypt -lz -lpthread -lcrypt -lm -o labelPrint
-
-# Taken from https://community.ubnt.com/t5/UniFi-Wireless/RPI-Dashbutton-Turn-RaspberryPI-with-Dymo-LabelWriter-into-a/td-p/1667513
+g++ `cups-config --cflags` labelPrinter.cpp `cups-config --libs` -o labelPrinter
